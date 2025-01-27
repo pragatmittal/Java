6f18fb30b93b176d45e6c7ebb8d6a9fcbde635b4
mittalpragat <mittalpragat@gmail.com>
2025-01-27 13:24:54 +0530

    quicky

---
+++ Enter/src/QuickSort.java
@@ -0,0 +1,39 @@
+package io.github.jiangdequan;
+
+public class QuickSort {
+    public static void main(String[] args){
+        int [] arr={432,4,242,4,24,23423,423,4,24,24};
+        sort(arr,0,arr.length);
+        system.out.println(Arrays.toString(arr));
+
+    } 
+    static void sort(int []nums,int low,int high){
+        if(low<=high){
+            return ;
+
+        }
+        int s=low;
+        int e =high;
+        int mid =s+(e-s)/2;
+        int pivot =nums[mid];
+        while(s<=e){
+            while(nums[s]<pivot){
+                s++;
+            }
+            while(nums[e]>pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp=nums[s];
+                nums[s]=nums[e];
+                nums[e]=temp;
+                s++;
+                e--;
+
+            }
+        }
+        sort(nums,low,e);
+        sort(nums,s,high);
+
+    }
+}
