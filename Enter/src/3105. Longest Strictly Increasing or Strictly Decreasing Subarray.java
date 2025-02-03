03f5b12cae1a8b6c949d2419d7868e5e840e9222
mittalpragat <mittalpragat@gmail.com>
2025-02-03 23:45:59 +0530

    3105.Longest Strictly Inc or Strictly Dec Subarray

---
+++ Enter/src/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.java
@@ -0,0 +1,30 @@
+package io.github.jiangdequan;
+
+public class 3105. Longest Strictly Increasing or Strictly Decreasing Subarray {
+    class Solution {
+        public int longestMonotonicSubarray(int[] nums) {
+            int inc=1;
+            int dec=1;
+            int count=1;
+    
+            for(int i=0;i<nums.length-1;i++){
+                if(nums[i]<nums[i+1]){
+                 inc++;
+                 dec=1;   
+                }
+                else if(nums[i]>nums[i+1]){
+                    dec++;
+                    inc=1;
+                }
+    
+                else{
+                    inc=1;
+                    dec=1;
+    
+                }
+                count=Math.max(count,Math.max(inc,dec));
+            }
+            return count;
+        }
+    }
+}
