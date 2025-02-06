09823cb0e08f71b1e0f2f148e5aabf0528120733
mittalpragat <mittalpragat@gmail.com>
2025-02-06 11:43:49 +0530

    1010.PairsofSongsWithTotalDurationsDivisibleby60

---
+++ Enter/src/1010.PairsofSongsWithTotalDurationsDivisibleby60.java
@@ -0,0 +1,22 @@
+package io.github.jiangdequan;
+
+public class 1010.PairsofSongsWithTotalDurationsDivisibleby60 {
+    class Solution {
+        public int numPairsDivisibleBy60(int[] time) {
+            int count = 0;
+            HashMap<Integer, Integer> mapping = new HashMap<>();
+    
+            for (int i=0;i<time.length;i++) {
+                int t=time[i];
+                if (t % 60 == 0) {
+                    count += mapping.getOrDefault(0, 0);
+                } else {
+                    count += mapping.getOrDefault(60 - t % 60, 0);
+                }
+                mapping.put(t % 60, mapping.getOrDefault(t % 60, 0) + 1);
+            }
+            return count;
+        }
+    }
+    
+}
