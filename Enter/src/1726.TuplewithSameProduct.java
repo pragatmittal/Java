0f08b65a931f93a73913029f16bc5b3f62dd98e5
mittalpragat <mittalpragat@gmail.com>
2025-02-06 11:13:33 +0530

    1726.TuplewithSameProduct

---
+++ Enter/src/1726.TuplewithSameProduct.java
@@ -0,0 +1,25 @@
+package io.github.jiangdequan;
+
+public class 1726.TuplewithSameProduct {
+    class Solution {
+        public int tupleSameProduct(int[] nums) {
+            int count=0;
+            
+            HashMap<Integer,Integer>mapping=new HashMap<>();
+    
+            for(int i=0;i<nums.length;i++){
+               for(int j=i+1;j<nums.length;j++){
+                int product=nums[i]*nums[j];
+                if(mapping.containsKey(product)){
+                    int freq=mapping.get(product);
+    
+                    count+=8*freq;
+    
+                }
+                mapping.put(product,mapping.getOrDefault(product,0)+1);
+               }
+            }
+            return count;
+        }
+    }
+}
