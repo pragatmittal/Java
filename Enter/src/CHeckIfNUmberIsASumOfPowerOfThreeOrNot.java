290adc66fed095bd50053a3dbf4b247651fe44a7
mittalpragat <mittalpragat@gmail.com>
2025-03-04 10:20:25 +0530

    Add solution to check if a number is a sum of powers of three

---
+++ Enter/src/CHeckIfNUmberIsASumOfPowerOfThreeOrNot.java
@@ -0,0 +1,18 @@
+package io.github.jiangdequan;
+
+public class CHeckIfNUmberIsASumOfPowerOfThreeOrNot {
+    class Solution {
+        public boolean checkPowersOfThree(int n) {
+            while(n>0){
+                if(n%3==2){
+                    return false ;
+    
+                }
+                n/=3;
+    
+            }
+            return true;
+        }
+    
+    }
+}
